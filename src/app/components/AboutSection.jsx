62aa62efc30e472eaa0d162e85499ab5eabfc709
Devex35 <exanton904@gmail.com>
2024-07-16 16:17:15 +0200

    text update

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -57,7 +57,7 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          Welcome! I&apos;m Johanna &lsquo;a passionate and experienced online English tutor committed to helping students of all ages and backgrounds achieve fluency and confidence in English. With an extensive background in the language&lsquo; I offer personalized and engaging lessons tailored to each student&apos;s unique needs and goals. Whether you&apos;re preparing for exams&lsquo; improving your conversational skills, or advancing your professional communication&lsquo; I&apos;m here to support your journey. Let&apos;s work together to unlock your full potential and achieve your language learning objectives.
+          Welcome! I&apos;m Johanna a passionate and experienced online English tutor committed to helping students of all ages and backgrounds achieve fluency and confidence in English. With an extensive background in the language I offer personalized and engaging lessons tailored to each student&apos;s unique needs and goals. Whether you&apos;re preparing for exams improving your conversational skills, or advancing your professional communication I&apos;m here to support your journey. Let&apos;s work together to unlock your full potential and achieve your language learning objectives.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
